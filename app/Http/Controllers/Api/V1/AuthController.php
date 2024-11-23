b98c0770be7ca63c1122747901d1185c04809b99
d.mozhaev <d.mozhaev@notamedia.com.ru>
2024-11-23 18:48:31 +0300

    0002 | api start

---
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthRequest;
+use App\Http\Requests\RegisterRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(AuthRequest $request)
+    {
+
+    }
+
+    public function register(RegisterRequest $request)
+    {
+
+    }
+}
